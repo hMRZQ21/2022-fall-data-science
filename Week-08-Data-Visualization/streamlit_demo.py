79c1d0386ed915094d31ef4a699663cd8635a449
Hussam Marzooq <hussammarzooq21@gmail.com>
2022-10-29 19:27:28 -0400

    completed visualization hw

--- Week-08-Data-Visualization/streamlit_demo.py
+++ Week-08-Data-Visualization/streamlit_demo.py
@@ -24,23 +24,23 @@ st.code("""
 		""")
 
 # st.button('Click me')
-# st.checkbox('I agree')
-# st.radio('Pick one', ['cats', 'dogs'])
-# st.selectbox('Pick one', ['cats', 'dogs'])
-# st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
-# st.slider('Pick a number', 0, 100)
-# st.select_slider('Pick a size', ['S', 'M', 'L'])
-# st.text_input('First name')
-# st.number_input('Pick a number', 0, 10)
+st.checkbox('I agree')
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
+st.text_input('First name')
+st.number_input('Pick a number', 0, 10)
 
-# st.text_area('Text to translate')
+st.text_area('Text to translate')
 
-# st.date_input('Your birthday')
+st.date_input('Your birthday')
 
-# st.time_input('Meeting time')
+st.time_input('Meeting time')
 
-# st.file_uploader('Upload a CSV')
+st.file_uploader('Upload a CSV')
 
-# st.camera_input("Take a picture")
+st.camera_input("Take a picture")
 
-# st.color_picker('Pick a color')
+st.color_picker('Pick a color')
